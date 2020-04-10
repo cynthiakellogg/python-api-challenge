54bdd8642710406e543142bdf4e58cdcaaaf2de3
Cynthia <59157682+cynthiakellogg@users.noreply.github.com>
2020-04-10 13:06:19 -0700

    refactoring code

--- VacationPY/config.py
+++ VacationPY/config.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "Your Key Here!"
+weather_api_key = "faf1cf6cae1a0fd2f41fb1b4b2679057"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAyxEseuV4aaExiGr6__e9-ab4zgXm3BDw"
